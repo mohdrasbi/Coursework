d33ce845483d26edd12d6482f64393c655a49fd4
mohdrasbi <mohdrasbi123@gmail.com>
2019-02-03 16:33:05 -0700

    .

---
+++ csci3010-game_dev/week3/FirstUnityScript/Assets/Scripts/PrintMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Print a message
+/// </summary>
+public class PrintMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print supportive message
+        print("Hi, n00b!");
+    }
+
+}
