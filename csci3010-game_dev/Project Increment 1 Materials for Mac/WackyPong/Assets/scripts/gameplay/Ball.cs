84a32c412bf0564600f04d58149aebddb601f9f7
mohdrasbi <mohdrasbi123@gmail.com>
2019-02-11 01:45:27 -0700

    .

---
+++ csci3010-game_dev/Project Increment 1 Materials for Mac/WackyPong/Assets/scripts/gameplay/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A ball
+/// </summary>
+public class Ball : MonoBehaviour
+{
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+		
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+		
+	}
+}
---
+++ csci3010-game_dev/Project Increment 1 Materials for Mac/WackyPong/Assets/scripts/gameplay/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script used on the paddles
+/// </summary>
+public class Paddle : MonoBehaviour
+{
+	
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+        Rigidbody2D rb = gameObject.AddComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+		
+	}
+	
+}
